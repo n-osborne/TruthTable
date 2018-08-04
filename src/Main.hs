87b392619149046ecdafddea55e1ebcf6b2241d7
Nicolas Osborne <nicolas.osborne@etudiant.univ-lille1.fr>
2018-08-04 16:40:05 +0200

    Main program working

--- src/Main.hs
+++ src/Main.hs
@@ -8,14 +8,16 @@ Maintainer  : nicolas.osborne@etu.univ-lille1.fr
 -}
 module Main where
 
-import System.Environment
+import Data.Maybe
 import TruthTable.WellFormedFormula
 import TruthTable.PropLogTree
 import TruthTable.PropLogValuation
 
-main = do
-  arg <- getArgs
-  if (length arg) /= 1
-    then putStrLn "Error"
-    else do (map putStrLn truthtable)
-              where truthtable = truthTable (fmap buildTree (parsePL (head arg))) (fmap truthValuesArr (fmap setOfNames (parsePL (head arg))) []) 
+main = do putStrLn "Please type in your logical proposition"
+          prop <- getLine
+          let wff = fromMaybe [] (parsePL prop) 
+          let tree = buildTree wff
+          let setBool = truthValuesArr (setOfNames wff) []
+          let tt = truthTable tree setBool
+          mapM_ print tt
+
