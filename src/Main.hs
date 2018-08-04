68f4e2189cb34c946ba1b547eb0af548bd600d7a
Nicolas Osborne <nicolas.osborne@etudiant.univ-lille1.fr>
2018-08-04 18:41:15 +0200

    get prop via command line argument

--- src/Main.hs
+++ src/Main.hs
@@ -8,19 +8,24 @@ Maintainer  : nicolas.osborne@etu.univ-lille1.fr
 -}
 module Main where
 
+import System.Environment
 import Data.Maybe
 import TruthTable.WellFormedFormula
 import TruthTable.PropLogTree
 import TruthTable.PropLogValuation
 
-main = do putStrLn "Please type in your logical proposition"
-          prop <- getLine
-          let prop' = parsePL prop
-          if prop' == Nothing
+main = do args <- getArgs
+          if (length args) /= 1
             then putStrLn "Error"
-            else do let wff = (fromMaybe [] prop') 
-                    let tree = buildTree wff
-                    let setBool = truthValuesArr (setOfNames wff) []
-                    let tt = truthTable tree setBool
-                    mapM_ print tt
+            else do
+            let prop = head args
+            let prop' = parsePL prop
+            if prop' == Nothing
+              then putStrLn "Error"
+              else do
+              let wff = (fromMaybe [] prop') 
+              let tree = buildTree wff
+              let setBool = truthValuesArr (setOfNames wff) []
+              let tt = truthTable tree setBool
+              mapM_ print tt
 
